0aa6ea32e95e6760a459f9edaedd69992c7cfae9
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2024-11-15 19:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	let ans=""
+	let list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789"
+	for(let i=0;i<l;i++){
+		ans +=list.charAt(Math.floor(Math.random()*list.length))
+	}
+	return ans;
 }
 
 // Do not change the code below.
